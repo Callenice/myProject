0d642534ad08fa2e4befdf898f89787d9ccf1334
Carl <cgroning@kth.se>
2024-05-08 23:16:34 +0200

    Added tables Added reset for fields username, password added text inticating user. Added test customer

--- src/main/java/webinterface/webinteractor/Controller.java
+++ src/main/java/webinterface/webinteractor/Controller.java
@@ -1,9 +1,23 @@
 package webinterface.webinteractor;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import webinterface.webinteractor.Model.*;
 public class Controller {
+    @FXML
+    private TableView<Customer> customerTableView;
+
+    @FXML
+    private TableColumn<Customer, Long> pnoColumn;
+
+    @FXML
+    private TableColumn<Customer, String> firstNameColumn;
+
+    @FXML
+    private TableColumn<Customer, String> lastNameColumn;
+
     @FXML
     private Button logInButton;
     @FXML
@@ -17,16 +31,21 @@ public class Controller {
 
     private boolean isLoggedIn = false;
     private dbCommunication database;
+    private ObservableList<Customer> customers;
     public Controller() {
         this.database = new dbCommunication();
+
+        Customer customer = new Customer(9904061234L, "Carl", "Gröning");
+        customers = FXCollections.observableArrayList();
     }
 
     public void logInButtonClicked() throws DbException {
         if (!isLoggedIn) {
             System.out.println("User logged in...");
-            logInButton.setText("logged in");
+            logInButton.setText("logged in as:");
             String username = usernameField.getText();
             String password = passwordField.getText();
+            logInButton.setText("logged in as: "+username);
             System.out.println("Username: " + username);
             System.out.println("Password: " + password);
             usernameField.setVisible(false);
@@ -38,12 +57,15 @@ public class Controller {
             System.out.println("Already logged in.");
         }
     }
-    public void logOutMenuClicked(){
+    public void logOutMenuClicked() throws DbException {
         usernameField.setVisible(true);
         passwordField.setVisible(true);
         logInButton.setText("log In");
         isLoggedIn = false;
         logOutMenuObj.setVisible(false);
+        database.disconnect("library");
+        usernameField.setText("");
+        passwordField.setText("");
     }
 
 
