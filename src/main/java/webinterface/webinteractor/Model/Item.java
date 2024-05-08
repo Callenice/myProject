fadb3802c6ed713b8fb2761778688ba73ad37337
Carl <cgroning@kth.se>
2024-05-08 23:18:40 +0200

    Added skeleton for an order and item object.

---
+++ src/main/java/webinterface/webinteractor/Model/Item.java
@@ -0,0 +1,11 @@
+package webinterface.webinteractor.Model;
+
+public class Item {
+    final private String itemName;
+    final private int idNumber;
+
+    public Item(String itemName, int idNumber) {
+        this.itemName = itemName;
+        this.idNumber = idNumber;
+    }
+}
